ccc59112a9f41e8cd095ad68509f2836133d6296
Andrew Reyna <1711ajreyna@gmail.com>
2024-05-20 11:47:38 -0700

    updates

--- issues/views.py
+++ issues/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.views.generic import ListView, UpdateView
+from .models import Issue, Status
+from django.urls import reverse_lazy
 
-# Create your views here.
+class BoradView(ListView):
+    template_name = 'issues/board.html'
+    model = Issue
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        to_do = Status.objects.get(name='to do')
+        in_prog = Status.objects.get(name='in progress')
+        done = Status.objects.get(name='done')
+        context['to_do_list'] = Issue.objects.filter(
+            status=to_do
+        ).order_by('created_on').reverse()
+        context['in_prog_list'] = Issue.objects.filter(
+            status=in_prog
+        ).order_by('created_on').reverse()
+        context['done_list'] = Issue.objects.filter(
+            status=done
+        ).order_by('created_on').reverse()
+        return context 
+    
+class StatusUpdateView(UpdateView):
+    template_name = 'issues/edit.html'
+    model = Issue
+    fields = ['status']
+    success_url = reverse_lazy('board')
